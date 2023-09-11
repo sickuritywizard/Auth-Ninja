1d65f9078abd0de498437cc98269cb5590dcc407
killeroo7 <prajeet67@gmail.com>
2023-09-11 23:19:45 +0530

    Fixed Spelling

--- core/constants.py
+++ core/constants.py
@@ -2,7 +2,7 @@ import os
 
 #DEFAULT VALUES FOR PATH VARIABLES
 scriptDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))        #os.path.dirname twice to go ../
-PATH_VARIABLES_FILE = os.path.join(scriptDir , "pathVaribles.json")
+PATH_VARIABLES_FILE = os.path.join(scriptDir , "pathVariables.json")
 PATH_VARIABLE_DEFAULT_VALUE = "Pradeep"
 
 
@@ -11,7 +11,7 @@ xInteger = 123123
 xString = "stringParam"
 xBoolean = "True"
 xArrayQueryParam = "arrayQueryParam"
-xArrayReqBody = ['item1','item2']  #WONT WORK FOR "GET" IG ==> COZ ARRAY IN GET IS --> ?q=test&q=test2 (So for GET Array just give => stringSample?)
+xArrayReqBody = ['item1','item2'] 
 xFile = "File-Required"
 sampleValueDict = {"integer":xInteger,
 					"string":xString,
